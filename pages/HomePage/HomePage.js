ce34d8065c0b82ade2ebb9840932b2d72c12434c
Jack de Grave <jack.degrave@outlook.com>
2020-07-02 21:43:29 +0100

    feat: create layout component WIP

---
+++ pages/HomePage/HomePage.js
@@ -0,0 +1,4 @@
+import Layout from './Layout'
+export default function SpaceLaunch() {
+    return <Layout/>
+  }
---
+++ pages/HomePage/Layout.js
@@ -0,0 +1,3 @@
+export default function SpaceLaunch() {
+    return <h1>Layout</h1>
+  }
